161950bc74e6a1655db018be83a423feb8f1e3d0
Albert Rocha Tavares <gerenciamentoalberttavares@gmail.com>
2024-03-29 21:05:32 -0300

    First commit: Adicionando projeto Next.js

---
+++ components/Temporizador.tsx
@@ -0,0 +1,23 @@
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import styles from "../styles/Temporizador.module.css";
+
+interface TemporizadorProps {
+	key: any
+    duracao: number;
+    tempoEsgotado: () => void;
+}
+
+export default function Temporizador(props: TemporizadorProps) {
+    return (
+        <CountdownCircleTimer
+            duration={props.duracao}
+            size={120}
+            isPlaying
+            onComplete={props.tempoEsgotado}
+            colors={["#BCE596", "#F7B801", "#ED827A", "#ED827A"]}
+            colorsTime={[10, 6, 3, 0]}
+        >
+            {({ remainingTime }: { remainingTime: number }) => remainingTime }
+        </CountdownCircleTimer>
+    );
+}
